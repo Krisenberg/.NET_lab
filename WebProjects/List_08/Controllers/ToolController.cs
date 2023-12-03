e8fcd52141b03a748ef2c5b48604b9bfdd703f09
Krzysztof Glowacz <krzysztofglowacz88@gmail.com>
2023-12-03 22:07:42 +0100

    Beginning of the list 8.

---
+++ WebProjects/List_08/Controllers/ToolController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace List_08.Controllers
+{
+    public class ToolController : Controller
+    {
+        public IActionResult Solve(int a, int b, int c)
+        {
+            ViewBag.aParameter = a;
+            ViewBag.bParameter = b;
+            ViewBag.cParameter = c;
+            return View();
+        }
+    }
+}
