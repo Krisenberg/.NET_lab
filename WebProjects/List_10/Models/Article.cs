1432d06add36a7147785e2c0ce54db0815d59038
Krzysztof Glowacz <krzysztofglowacz88@gmail.com>
2023-12-16 20:04:37 +0100

    Started working on list no 10.

---
+++ WebProjects/List_10/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace List_10.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{13}$")]
+        public string EAN13 { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Article's name must be at least 3 characters long")]
+        [MaxLength(20, ErrorMessage = "Article's name must be at most 20 characters long")]
+        public string Name { get; set; }
+    }
+}
---
+++ WebProjects/List_10/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace List_10.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
